169a84542186e9f01f5727831d9b51dd64db198b
Kevyn <kworbetz@gmail.com>
2023-12-06 12:50:25 -0700

    fix bugs and adds ending

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,13 @@ var doorLeft = $("#Door-Left1");
 var doorCenter = $("#Door-Center1");
 var doorRight = $("#Door-Right1");
 var exit = $("#Exit");
+var msg = $("#count");
 
 exit.hide();
 KeyOne.hide();
 KeyTwo.hide();
 
 console.log(keyCount);
-
 var randomKey = Math.random() < 0.5 ? KeyOne : KeyTwo;
 randomKey.hide();
 
@@ -20,12 +20,14 @@ KeyOne.on("click", function () {
   console.log("Key00 clicked");
   KeyOne.hide();
   keyCount++;
+  checkKeyCount();
 });
 
 KeyTwo.on("click", function () {
   console.log("Key01 clicked");
   KeyTwo.hide();
   keyCount++;
+  checkKeyCount();
 });
 
 doorLeft.on("click", function () {
@@ -35,6 +37,7 @@ doorLeft.on("click", function () {
   }
   console.log(keyCount);
   RoomCount++;
+  GetRoomCount();
 });
 
 doorCenter.on("click", function () {
@@ -44,6 +47,7 @@ doorCenter.on("click", function () {
   }
   console.log(keyCount);
   RoomCount++;
+  GetRoomCount();
 });
 
 doorRight.on("click", function () {
@@ -53,12 +57,25 @@ doorRight.on("click", function () {
   }
   console.log(keyCount);
   RoomCount++;
+  GetRoomCount();
 });
 
-if (keyCount >= 10) {
-  exit.show();
-  console.log("Exit shown");
-  doorCenter.on("click", function () {
-    $("#GameBoard").hide();
-  });
+function checkKeyCount() {
+  if (keyCount >= 10) {
+    exit.show();
+    console.log("Exit shown");
+    doorCenter.on("click", function () {
+      $("#GameBoard").hide();
+      doorRight.hide();
+      doorLeft.hide();
+
+      msg
+        .text("you have Escaped The back Room??? ")
+        .css({ "font-size": "50px", color: "green", padding: "150px 0 0 0;" });
+    });
+  }
+}
+
+function GetRoomCount() {
+  msg.text("Welcome to Room " + RoomCount);
 }
